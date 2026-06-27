9cf88f6e6435e7d9cbabcaf09ff8d8d201c490ec
Narmatha0106 <narmathacse2007@gmail.com>
2026-06-27 12:25:39 +0530

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,25 @@
+#rock-paper-scissors
+import random
+choices=["rock","paper","scissors"]
+user_score=0
+computer_score=0
+while True:
+    print("\n~~~~~~ROCK PAPER SCISSORS~~~~~~~~")
+    user=input("Enter rock,paper,scissors:").lower()
+    if user not in choices:
+        print("Invalid input")
+        continue
+    computer=random.choice(choices)
+    print("Computer Choice:",computer)
+    if user==computer:
+        print("It's a Tie!")
+    elif(user=="rock"and computer=="scissors")or(user=="paper"and computer=="rock")or(user=="scissors"and computer=="paper"):
+        print("You Win!")
+        user_score+=1
+    else:
+        print("Computer Wins!")
+        computer_score+=1
+    print(f"Score->You:{user_score}|Computer:{computer_score}")
+    play=input("Play Again?(yes/no): ")
+    if play.lower()!="yes":
+       break
